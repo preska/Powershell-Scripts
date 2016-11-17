ea2edcb3a850d0de0e55874ad4008651cd0bef8e
preska <preska@users.noreply.github.com>
2016-11-17 16:43:41 -0600

    Update getSubnetRange.ps1

--- getSubnetRange.ps1
+++ getSubnetRange.ps1
@@ -1,20 +1,59 @@
-function getSubnetRange($IP, $netmask)
-\{
-	$IPBinary = [string]::join(".",($ip.split(".") | % {[System.Convert]::ToString($IP,2).PadLeft(8,"0")}))						# convert the IP address to binary
-	$netmaskBinary = [string]::join(".",($ip.split(".") | % {[System.Convert]::ToString($NetMask,2).PadLeft(8,"0")}))			# convert the NetMask to binary
-	
-	$networkAddress = (IPBinary -and netmaskBinary)																				# the bottom end of the range
-	
-	$range = -Not netmaskBinary																									# the size of the range
-	
-	$upperEndNetworkAddress = networkAddress + range																			# the upper limit of the range
-	
-	$networkAddress = [string]::join(".",($sbin.split(".") | % {[System.Convert]::ToByte($networkAddress,2)}))					# convert the lower limit back to decimal
-	$upperEndNetworkAddress = [string]::join(".",($sbin.split(".") | % {[System.Convert]::ToByte($upperEndNetworkAddress,2)}))	# convert the upper limit back to binary
+function binaryAND ($num1, $num2)
+{   
+	$andResult = (0..31)
 
+	for ($i = 0; $i -le $num1.length; $i++)
+	{
+		if($num1[$i] -and $num2[$i])
+		{
+			$andResult[$i] = 1
+		}
+		else
+		{
+			$andResult[$i] = 0
+		}
+	}
+    return $andResult
+}
+
+function toBinary ($dottedDecimal)
+{ 
+	$dottedDecimal.split(".") | ForEach-Object {$binary=$binary + $([convert]::toString($_,2).padleft(8,"0"))} 
+	return $binary 
+} 
+
+function toDottedDecimal ($binary)
+{ 
+	do {$dottedDecimal += "." + [string]$([convert]::toInt32($binary.substring($i,8),2)); $i+=8 } while ($i -le 24) 
+	return $dottedDecimal.substring(1) 
+} 
+
+function getSubnetRange($IP, $netmask)
+{
+	# to binary
+	$IPBinary = toBinary -dottedDecimal $IP
+	$netmaskBinary = toBinary -dottedDecimal $netmask
 	
-	$arr = networkAddress, upperEndNetworkAddress
+	# TEST
+	write-host $IPBinary
+	write-host $netmaskBinary
+	$test = binaryAND -num1 $IPBinary -num2 $netmaskBinary
+	write-host $test
+
+	$networkAddress = ($IPBinary -and $netmaskBinary)		# the bottom end of the range
 	
-	return $arr																													# returns the two IP adresses	
+	$range = -Not ($netmaskBinary)							# the size of the range
 	
-\}
+	$upperEndNetworkAddress = $networkAddress + $range		# the upper limit of the range
+	
+	# to decimal
+	$networkAddress = toDottedDecimal -binary networkAddress
+	$upperEndNetworkAddress = toDottedDecimal -binary upperEndNetworkAddress
+
+	$arr = $networkAddress, $upperEndNetworkAddress
+
+	Write-host $("$arr")
+	#return $arr
+}
+
+getSubnetRange -IP "216.3.128.12" -netmask "255.255.255.0"
